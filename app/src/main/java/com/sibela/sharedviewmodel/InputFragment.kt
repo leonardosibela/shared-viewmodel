ae9d4248a6dfff2c16a902e03c5c32556fb4ed6d
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-31 11:31:06 -0300

    Implementing boilerplate from InputFragment

--- app/src/main/java/com/sibela/sharedviewmodel/InputFragment.kt
+++ app/src/main/java/com/sibela/sharedviewmodel/InputFragment.kt
@@ -1,22 +1,28 @@
 package com.sibela.sharedviewmodel
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sibela.sharedviewmodel.databinding.FragmentInputBinding
 
 class InputFragment : Fragment() {
+
+    private var _binding: FragmentInputBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_input, container, false)
+    ): View {
+        _binding = FragmentInputBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = InputFragment()
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
