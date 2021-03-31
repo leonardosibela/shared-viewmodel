c7b87ad30649954b278f28da41bfc986422e6936
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-31 11:23:29 -0300

    Implementing main activity

--- app/src/main/java/com/sibela/sharedviewmodel/MainActivity.kt
+++ app/src/main/java/com/sibela/sharedviewmodel/MainActivity.kt
@@ -2,10 +2,28 @@ package com.sibela.sharedviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.sibela.sharedviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
