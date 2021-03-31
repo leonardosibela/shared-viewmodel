5384126ad833846cf485e4c81594352bce68ea61
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-31 11:19:56 -0300

    Adding SharedViewModel

---
+++ app/src/main/java/com/sibela/sharedviewmodel/SharedViewModel.kt
@@ -0,0 +1,15 @@
+package com.sibela.sharedviewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+
+    private val _county = MutableLiveData("Brazil")
+    private val county: LiveData<String> = _county
+
+    fun saveCountr(newCountry: String) {
+        _county.value = newCountry
+    }
+}
